42d1f79b7b75c6a7a75a6d3486d25ed60c4f8ecf
tndms0308@naver.com <nhs503601@gmail.com>
2023-09-15 18:04:21 +0900

    변경

--- src/main/java/loginTest/loginfunction/controller/loginController.java
+++ src/main/java/loginTest/loginfunction/controller/loginController.java
@@ -4,6 +4,7 @@ import loginTest.loginfunction.domain.People;
 import loginTest.loginfunction.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.lang.reflect.Member;
@@ -19,7 +20,8 @@ public class loginController {
         this.loginService = loginService;
     }
 
-
+    // @GetMapping("/") -> / 는 url 이다
+    // return "homeForm"; homeForm -> html 파일이다
     @GetMapping("/")
     public String home() {
         return "homeForm";
@@ -32,11 +34,18 @@ public class loginController {
 
     @GetMapping("login")
     public String login() {
+
         return "loginForm";
+
+
     }
 
+    /*
+    @ModelAttribute 는 loginForm 에서 넘어오는 값들을 모두 하나의 객체로 만들어서
+    People의 가장 적게 사용하는 파라미터를 이용한 생성자를 사용해서 객체로 등록이 된다
+     */
     @PostMapping("login")
-    public String login(People people) {
+    public String login(@ModelAttribute People people) {
         loginService.login(people);
         return "loginForm";
     }
