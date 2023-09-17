347cb15d18f5f1b643e09af4499f4ec6a8af3d22
junyoung-choe <wjdghkswnsdu@naver.com>
2023-09-17 15:54:46 +0900

    9-15 cookie 사용

--- src/main/java/loginTest/loginfunction/controller/loginController.java
+++ src/main/java/loginTest/loginfunction/controller/loginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Member;
 import java.util.Arrays;
 
@@ -40,11 +42,46 @@ public class loginController {
     /*
     @ModelAttribute 는 loginForm 에서 넘어오는 값들을 모두 하나의 객체로 만들어서
     People의 가장 적게 사용하는 파라미터를 이용한 생성자를 사용해서 객체로 등록이 된다
+
+     HttpServletResponse response -> 브라우저에 어떠한 결과를 넘겨주기 위해서 사용한다
      */
     @PostMapping("login")
-    public String login(@ModelAttribute People people) {
-        loginService.login(people);
-        return "loginForm";
+    public String login(@ModelAttribute People people, HttpServletResponse response) {
+
+        // DB 연결후 사용해야 한다 !
+//        if (!loginService.login(people)) { // 로그인 구현 메소드로 이동한다 !
+//            return "loginForm"; //  해당 email이 없다면 다시 로그인 페이지로 이동한다
+//
+//            // 아이디 비밀번호를 다시 입력하시오
+//        }
+
+
+        // 로그인 구현은 가능하나 로그아웃의 기능을 구현할때 DB에 해당 멤버를 지워버릴수 없다 !
+        // 따라서 뭔가 로그인 했다는 표시와 그 표시를 지우는 과정으로  로그인을 구현해야 한다 !
+        // -> 쿠키를 사용한 로그인 처리 ( 위에서 해당 사람이 로그인이 가능하다면 여기로 내려온다 )
+
+        // html 파일에서 넘어온 이메일의 이름으로 세션을 걸어준다 !
+        Cookie cookie = new Cookie("email", people.getEmail());
+
+        // 쿠키 시간을 디폴트로 설정한다.
+        //cookie.setMaxAge();
+
+        // 서버에 해당 유저의 쿠키 정보를 넘겨준다
+        response.addCookie(cookie);
+
+
+        return "homeForm";
     }
+
+
+    /*
+    @GetMapping("")
+
+    Cookie cookie = new Cookie("emain", null);
+    cookie.setMaxAge(0);
+    response.addCookie(cookie);
+
+     */
+
 
 }
--- src/main/java/loginTest/loginfunction/service/LoginService.java
+++ src/main/java/loginTest/loginfunction/service/LoginService.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    People people = new People();
+    public boolean login(People p) {
 
-    public People login(People p) {
+//        people.setEmail(p.getEmail());
+//        people.setPassword(p.getPassword());
+//        System.out.println(p.getEmail() + p.getPassword());
 
-        people.setEmail(p.getEmail());
-        people.setPassword(p.getPassword());
+        // 넘어온 p 객체와 db에 일치하는 ID와 비밀번호가 있다면 로그인 성공 !
 
-        System.out.println(p.getEmail() + p.getPassword());
-        return people;
+        // 해당 p 객체를 Repository 에 넘겨주고 해당 객체가 존재하는지 확인해야한다 !
+
+        return false;
     }
 }
